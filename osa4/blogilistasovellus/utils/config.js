da32b55a8aadeb30c4c479059ba098c479e26b31
Touko Pakarinen <touko@meili.fi>
2022-01-07 04:28:32 +0200

    4.8.

--- osa4/blogilistasovellus/utils/config.js
+++ osa4/blogilistasovellus/utils/config.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 let PORT = process.env.PORT;
-let MONGO_URI = process.env.MONGO_URI;
+let MONGO_URI =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGO_URI
+    : process.env.MONGO_URI;
 
 module.exports = { MONGO_URI, PORT };
