28a99c663ae950350484a9b686dffc5931f03ae3
Touko Pakarinen <touko@meili.fi>
2022-01-08 21:21:54 +0200

    4.15., 4.17. - 4.19.

--- osa4/blogilistasovellus/app.js
+++ osa4/blogilistasovellus/app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const mongoose = require("mongoose");
 
 mongoose.connect(config.MONGO_URI);
@@ -10,4 +12,6 @@ mongoose.connect(config.MONGO_URI);
 app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 module.exports = app;
--- osa4/blogilistasovellus/models/blog.js
+++ osa4/blogilistasovellus/models/blog.js
@@ -5,6 +5,10 @@ const blogSchema = mongoose.Schema({
   author: String,
   url: String,
   likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 const Blog = mongoose.model("Blog", blogSchema);
